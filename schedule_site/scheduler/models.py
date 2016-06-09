caa976b977ac4f8269a8a52cbc35796a49019b76
Janis <janis@jaggedverge.com>
2016-06-09 22:31:43 +1000

    Created models for Employee/Day

--- schedule_site/scheduler/models.py
+++ schedule_site/scheduler/models.py
@@ -13,9 +13,40 @@ AVAILABLE_ROLES = (
     ('3', DISHWASHER),
 )
 
+MONDAY = 'Monday'
+TUESDAY = 'Tuesday'
+WEDNSEDAY = 'Wednseday'
+THURSDAY = 'Thursday'
+FRIDAY = 'Friday'
+SATURDAY = 'Saturday'
+SUNDAY = 'Sunday'
+
+DAYS = (
+    ('MON', MONDAY),
+    ('TUE', TUESDAY),
+    ('WED', WEDNSEDAY),
+    ('THU', THURSDAY),
+    ('FRI', FRIDAY),
+    ('SAT', SATURDAY),
+    ('SUN', SUNDAY),
+)
+
 class WorkRole(models.Model):
     """The role performed by an employee"""
     role = models.CharField(
         max_length=2,
         choices=AVAILABLE_ROLES,
+    )
+
+class Employee(models.Model):
+    """Information about an Employee"""
+    max_hours = models.IntegerField()
+    employee_id = models.IntegerField()
+    capable_roles = models.ManyToManyField(WorkRole)
+
+class Day(models.Model):
+    """Represent a day of the week"""
+    day = models.CharField(
+        max_length=3,
+        choices=DAYS,
     )
