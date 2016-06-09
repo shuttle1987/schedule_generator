0fc1a77e74487f4a98aa08932613c628494599a5
Janis <janis@jaggedverge.com>
2016-06-09 21:57:54 +1000

    Created WorkRole model and associated migrations

--- schedule_site/scheduler/models.py
+++ schedule_site/scheduler/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+MANAGER = 'Manager'
+CHEF = 'Chef'
+COOK = 'Cook'
+DISHWASHER = 'Dishwasher'
+
+AVAILABLE_ROLES = (
+    ('0', MANAGER),
+    ('1', CHEF),
+    ('2', COOK),
+    ('3', DISHWASHER),
+)
+
+class WorkRole(models.Model):
+    """The role performed by an employee"""
+    role = models.CharField(
+        max_length=2,
+        choices=AVAILABLE_ROLES,
+    )
