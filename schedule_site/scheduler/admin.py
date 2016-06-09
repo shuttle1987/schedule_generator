b3232afed3d4908b05092e55c62474fc5ab19238
Janis <janis@jaggedverge.com>
2016-06-09 22:31:57 +1000

    Register models in admin

--- schedule_site/scheduler/admin.py
+++ schedule_site/scheduler/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    WorkRole,
+    Employee,
+    Day,
+)
+
+admin.site.register(WorkRole)
+admin.site.register(Employee)
+admin.site.register(Day)
