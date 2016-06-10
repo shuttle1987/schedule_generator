98c30ff56521bf4ac38332e661154d264c94fc88
Janis <janis@jaggedverge.com>
2016-06-10 14:00:46 +1000

    Created view for staff import

--- schedule_site/scheduler/views.py
+++ schedule_site/scheduler/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+from .csv_handling import import_staff_from_csv
+
+def import_staff_info(request):
+    """Import information about staff availability and shift requirements"""
+    path_to_staff = "staff.csv"
+    import_staff_from_csv(path_to_staff)
+
+    template = loader.get_template('scheduler/import.html')
+    html = template.render(
+        { #todo render the staff that were just imported
+        },
+        request
+    )
+    return HttpResponse(html)
+
